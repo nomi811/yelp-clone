155844c8f5ecc8b32809de83b54de69da9550c36
acsauk <acsauk@gmail.com>
2016-08-22 17:54:20 +0100

    completes step #5

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,5 @@
 class RestaurantsController < ApplicationController
   def index
-    raise "hello from index"
+    
   end
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -4,8 +4,8 @@ feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
-      expect(page).to have_content 'No restauratns yet'
-      expect(page).to have_content 'Add a restaurant'
+      expect(page).to have_content 'No restaurants yet'
+      expect(page).to have_link 'Add a restaurant'
     end
   end
 end
