2f82ece6977471ade27676d42f9b4440912bb1cd
Nomi Vos <nvoswork@gmail.com>
2016-08-24 12:02:12 -0500

    tests pass when User and Resaurant associations are commented out

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,7 @@
 class ReviewsController < ApplicationController
 
+  # before_action :authenticate_user!
+
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
@@ -7,8 +9,8 @@ class ReviewsController < ApplicationController
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
-    # @restaurant.reviews.create(review_params)
-    @review = @restaurant.build_review review_params, current_user
+    # @restaurant.reviews.create_with_user(review_params)
+    @review = @restaurant.reviews.build_with_user review_params, current_user
 
     if @review.save
       redirect_to restaurants_path
@@ -21,11 +23,11 @@ class ReviewsController < ApplicationController
     end
   end
 
-  def build_review(attributes = {}, user)
-    review = reviews.build(attributes)
-    review.user = user
-    review
-  end
+  # def build_review(attributes = {}, user)
+  #   review = reviews.build(attributes)
+  #   review.user = user
+  #   review
+  # end
 
   private
 
