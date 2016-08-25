63ab52b5eec6fea474e6904625ad799adb1ba4d8
Nomi Vos <nvoswork@gmail.com>
2016-08-25 10:25:22 -0500

    WIP version3 section3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,16 @@
 class Restaurant < ActiveRecord::Base
+  include WithUserAssociationExtension
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+        -> { extending WithUserAssociationExtension },
+        dependent: :destroy
 
   validates :name, length: { minimum: 3 }, uniqueness: true
+
+  has_many :reviews do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
+  end
 end
